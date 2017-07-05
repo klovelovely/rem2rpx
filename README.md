5e0684c4ae583a42ecb27690a6ec211214c73abe
Kavalon Wong <klovelovely@live.com>
2017-07-05 19:27:48 +0800

    fix 1rem 在 while 循环中无法识别的问题, 原因详见 README

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # rem 转 rpx
 
-rem 单位转 rpx 小程序单位
+rem 单位批量转换为微信小程序专用的 rpx 单位
 
 # todo
 
-- [ ] 1rem 在 while 循环中无法识别的 bug
+- [x] 1rem 在 while 循环中无法识别的问题, 原因在于:
+
+    1.  当前正则表达式通过全局匹配标识符 //g 进行匹配时, 此正则表达式对象自身的 lastIndex 属性值会默认保存上次匹配时的 index, 然后每次的匹配都会从 lastIndex 开始.
+    
+        当然, 如果没有全局匹配标识符, 则每次匹配时的 lastIndex 总是为 0.
+        
+    2.  splice 替换导致要匹配的 sourceString 整体长度发生了变化
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// 配置 rem, 即移动端页面中 html 的 font-size
+var rem = 32;
+
+// 初始化必要信息
 var source = document.querySelector("#source");
-var strSource = source.value;
+var sourceString = source.value;
 var target = document.querySelector("#target");
 var reg = new RegExp(/(\d+\.\d+|\.\d+|\d+)rem/g);
 
@@ -7,21 +11,31 @@ function convertCode() {
     var i = 0, result, start, end, matchedValue, rpxValue;
 
     // rem 单位的值转换为 rpx
-    while ((result = reg.exec(strSource)) !== null) {
+    while ((result = reg.exec(sourceString)) !== null) {
+
         start = result.index;
         end = reg.lastIndex;
-        console.log(result, start, end);
-        // 用 - 3 移除 rem
-        matchedValue = Number(strSource.slice(start, end - 3));
+        console.log(JSON.stringify(result), start, end, sourceString.slice(start, end));
+
+        // 用 - 3 移除 rem 单位
+        matchedValue = Number(sourceString.slice(start, end - 3));
         // 多了一个 "* 2" 是由于除发现页之外, 之前所有页面使用的 rem 单位都是基于 750 设计图除以 2 得出的
-        rpxValue = matchedValue * 16 * 2;
-        strSource = strSource.split("");
-        strSource.splice(start, end - start, rpxValue + "rpx");
-        strSource = strSource.join("");
+        rpxValue = matchedValue * rem;
+        console.log('matchedValue 长度: ', matchedValue.toString().length, ' rpxValue 长度: ', rpxValue.toString().length);
+
+        sourceString = sourceString.split("");
+        sourceString.splice(start, end - start, rpxValue + "rpx");
+        sourceString = sourceString.join("");
+
+        reg.lastIndex += rpxValue.toString().length - matchedValue.toString().length;
+        console.log('修正后的 reg.lastIndex 为: ', reg.lastIndex);
+        console.warn(sourceString.slice(reg.lastIndex, reg.lastIndex + 20));
+        console.log('------------------------------------------------------------')
+
         i++;
     }
 
-    target.value = strSource;
+    target.value = sourceString;
     target.select();
 }
 
