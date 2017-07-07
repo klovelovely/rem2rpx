699f3787662c8c00499254b4a7d3c4855c66184f
Kavalon Wong <klovelovely@live.com>
2017-07-07 17:02:38 +0800

    fix 生成逻辑

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-// 配置 rem, 即移动端页面中 html 的 font-size
-var rem = 32;
+// 批量转换按钮
+document.querySelector("#convertButton").addEventListener("click", function () {
+    convertCode(document.querySelector("#source").value)
+});
 
-// 初始化必要信息
-var source = document.querySelector("#source");
-var sourceString = source.value;
-var target = document.querySelector("#target");
-var reg = new RegExp(/(\d+\.\d+|\.\d+|\d+)rem/g);
-
-function convertCode() {
-    var i = 0, result, start, end, matchedValue, rpxValue;
+// 批量转换 handler
+function convertCode(sourceString, options = {
+    rem: 32,
+    reg: new RegExp(/(\d+\.\d+|\.\d+|\d+)rem/g)
+}) {
+    let i = 0, result, start, end, matchedValue, rpxValue;
 
     // rem 单位的值转换为 rpx
-    while ((result = reg.exec(sourceString)) !== null) {
+    while ((result = options.reg.exec(sourceString)) !== null) {
 
         start = result.index;
-        end = reg.lastIndex;
-        console.log(JSON.stringify(result), start, end, sourceString.slice(start, end));
+        end = options.reg.lastIndex;
+        /*console.log(JSON.stringify(result), start, end, sourceString.slice(start, end));*/
 
-        // 用 - 3 移除 rem 单位
+        // 用 end - 3 移除数字后面的 rem 单位
         matchedValue = Number(sourceString.slice(start, end - 3));
-        // 多了一个 "* 2" 是由于除发现页之外, 之前所有页面使用的 rem 单位都是基于 750 设计图除以 2 得出的
-        rpxValue = matchedValue * rem;
-        console.log('matchedValue 长度: ', matchedValue.toString().length, ' rpxValue 长度: ', rpxValue.toString().length);
+        rpxValue = matchedValue * options.rem;
+        /*console.log('matchedValue 长度: ', matchedValue.toString().length, ' rpxValue 长度: ', rpxValue.toString().length);*/
 
         sourceString = sourceString.split("");
         sourceString.splice(start, end - start, rpxValue + "rpx");
         sourceString = sourceString.join("");
 
-        reg.lastIndex += rpxValue.toString().length - matchedValue.toString().length;
-        console.log('修正后的 reg.lastIndex 为: ', reg.lastIndex);
-        console.warn(sourceString.slice(reg.lastIndex, reg.lastIndex + 20));
-        console.log('------------------------------------------------------------')
+        options.reg.lastIndex += rpxValue.toString().length - matchedValue.toString().length;
+        /*console.log('修正后的 options.reg.lastIndex 为: ', options.reg.lastIndex);
+         console.warn(sourceString.slice(options.reg.lastIndex, options.reg.lastIndex + 20));
+         console.log('------------------------------------------------------------')*/
 
         i++;
     }
 
-    target.value = sourceString;
-    target.select();
-}
+    let target = document.querySelector("#target");
+    if (target) {
+        target.value = sourceString;
+        target && target.select();
+    }
 
-document.querySelector("#convertButton").addEventListener("click", convertCode);
+    return sourceString;
+}
